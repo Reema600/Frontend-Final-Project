0163a31d708f8a7494a51ba411eb7967e117579c
Reema <reema600@hotmail.com>
2023-06-14 14:54:41 +0300

    uupdate

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Login from './Component/Login'
 import NavBar from './Component/NavBar'
 import Home from './Component/Home'
+// import SearchBar from "./Component/SearchBar";
 
 function App() {
   
@@ -10,10 +11,11 @@ function App() {
   return (
     <>
    <NavBar></NavBar>
-   <search></search>
+   
     <Routes>
       <Route path='/' element={<Login></Login>}></Route>
       <Route path='/home' element={<Home></Home>}></Route>
+      {/* <Route path='/SearchBar' element={<SearchBar></SearchBar>}></Route> */}
       </Routes>  
     </>
   )
